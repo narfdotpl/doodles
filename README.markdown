8ead492bc2be313375a6bda7ef88e977c4484f4c
Maciej Konieczny <hello@narf.pl>
2011-01-01 23:04:54 +0100

    added multi-predicate filter()

--- README.markdown
+++ README.markdown
@@ -3,8 +3,10 @@ doodles
 
 hacks that may just be abstract shapes
 
+ - [`mfilter()`][mfilter] -- multi-predicate `filter()`
  - [`@not_your_camelcase_test_case`][nyctc] -- `unittest.TestCase` decorator
 
+  [mfilter]: https://github.com/narfdotpl/doodles/blob/master/doodles/mfilter.py
   [nyctc]: https://github.com/narfdotpl/doodles/blob/master/doodles/nyctc.py
 
 
---
+++ doodles/mfilter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+Multi-predicate filter().
+"""
+
+__author__ = 'Maciej Konieczny <hello@narf.pl>'
+
+
+def mfilter(*args):
+    *predicates, iterable = args
+    for item in iterable:
+        for predicate in predicates:
+            if not predicate(item):
+                break
+        else:  # if no break
+            yield item
+
+
+def _main():
+    paths = ['.', '..', 'dotfiles/', 'pgadmin.log', '.vim/']
+    isdir = lambda string: string.endswith('/')
+
+    # ugly way
+    normal_dirs = [path for path in paths
+                   if isdir(path) and not path.startswith('.')]
+    print(normal_dirs)
+
+    # functional way
+    doesnt = lambda func: lambda *args, **kwargs: not func(*args, **kwargs)
+    startswith = lambda prefix: lambda string: string.startswith(prefix)
+    normal_dirs = list(mfilter(isdir, doesnt(startswith('.')), paths))
+    print(normal_dirs)
+
+if __name__ == '__main__':
+    _main()
