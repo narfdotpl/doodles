09d8b6fcbf75ddc722d03ceeab74a7e460127fc7
Maciej Konieczny <hello@narf.pl>
2014-08-19 08:13:07 +0200

    tested Swift doodles in Xcode 6 Beta 6

--- doodles/chain.swift
+++ doodles/chain.swift
@@ -9,7 +9,7 @@
 //     h(g(f(x)))
 //
 //
-// Tested in Xcode 6 Beta 5.
+// Tested in Xcode 6 Beta 6.
 
 
 
--- doodles/custom_while.swift
+++ doodles/custom_while.swift
@@ -5,7 +5,7 @@
 // http://matt.might.net/articles/implementing-laziness/
 // :)
 //
-// Tested in Xcode 6 Beta 5.
+// Tested in Xcode 6 Beta 6.
 
 func customWhile(condition: @autoclosure () -> Bool, body: () -> ()) {
     if condition() {
